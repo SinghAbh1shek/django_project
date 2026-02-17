5e77bbe56889df5b277683063ce2338625254fe2
Abhishek Singh <abhisheksingh.muj@gmail.com>
2026-02-17 14:53:19 +0530

    added custome user model and registration logic

--- 03_community/users/models.py
+++ 03_community/users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+
+    profile = models.ImageField(upload_to='profile/', null=True, blank=True)
+    bio = models.TextField(max_length=200, null=True, blank=True)
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    def __str__(self):
+        return f" {self.username} - {self.email}"
+ 
+
+
--- 03_community/users/views.py
+++ 03_community/users/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.contrib.auth import get_user_model, authenticate, login
+
+User = get_user_model()
 
 @login_required(login_url='login')
 def profile(requests):
@@ -10,22 +13,45 @@ def login_page(request):
     return render(request, 'users/login.html')
 
 def register_page(request):
+    if request.user.is_authenticated:
+        return redirect('feed')
+    
     if request.method == 'POST':
+        profile = request.FILES.get('pfp')
         first_name = request.POST.get('first_name')
         last_name = request.POST.get('last_name')
         username = request.POST.get('username')
         email = request.POST.get('email')
         password = request.POST.get('password')
 
-        if not first_name or last_name or username or email or password:
+
+        if not first_name or not last_name or not username or not email or not password:
             messages.error(request, "Note: All fields marked with * are required")
             return redirect('register')
+        
+        if User.objects.filter(username = username).exists() or User.objects.filter(email = email).exists():
+            messages.error(request, "Note: username or email already exist")
+            return redirect('register')
+        
+        user = User.objects.create_user(
+            username=username,
+            first_name = first_name,
+            last_name = last_name,
+            email=email,
+            password=password
+        )
+        
+        if profile:
+            user.profile = profile
+            user.save()
 
-        print(first_name)
-        print(last_name)
-        print(username)
-        print(email)
-        print(password)
-        messages.success(request, "account created")
+        user_obj = authenticate(request, username = username, password = password)
+        if user_obj:
+            messages.success(request, "account created")
+            login(request, user_obj)
+            return redirect('feed')
+        
+        messages.error(request, "something goes wrong")
         return redirect('register')
+
     return render(request, 'users/register.html')
