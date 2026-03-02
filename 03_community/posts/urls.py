8c169d177d5e3a24dbe9b84362f5e6912dfc29e7
Abhishek Singh <abhisheksingh.muj@gmail.com>
2026-03-02 14:19:24 +0530

    added create post

--- 03_community/posts/urls.py
+++ 03_community/posts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', feed, name='feed')
+    path('', feed, name='feed'),
+    path('create-post/', create_post, name='create_post'),
 ]
--- 03_community/posts/views.py
+++ 03_community/posts/views.py
@@ -4,3 +4,7 @@ from django.contrib.auth.decorators import login_required
 @login_required(login_url='login')
 def feed(request):
     return render(request, 'posts/feed.html')
+
+@login_required(login_url='login')
+def create_post(request):
+    return render(request, 'posts/create_post.html')
