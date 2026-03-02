df566a29ee61a725a83f473c7145c87541e67349
Abhishek Singh <abhisheksingh.muj@gmail.com>
2026-03-02 14:28:29 +0530

    added post model

--- 03_community/posts/models.py
+++ 03_community/posts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='posts')
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='post_images/', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title[:10]} - {self.user.username}"
+    
+    class Meta:
+        ordering = ['-created_at']
--- 03_community/users/models.py
+++ 03_community/users/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
 
     profile = models.ImageField(upload_to='profile/', default='profile/default.jpg', blank=True)
-    bio = models.TextField(max_length=200, null=True, blank=True)
+    bio = models.TextField(max_length=200, blank=True)
     email = models.EmailField(unique=True)
 
     USERNAME_FIELD = 'username'
